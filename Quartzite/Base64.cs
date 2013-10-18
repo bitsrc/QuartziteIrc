dd9e88f1c5176dad5174fe62a13002e0ee516418
phate408 <phate408@gmail.com>
2013-10-18 03:40:08 -0700

    Added Base64 IP decoding/encoding.

---
+++ Quartzite/Base64.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+
+namespace Quartzite
+{
+    public class Base64
+    {
+        public static String EncodeTimestamp(int timestamp)
+        {
+            return "";
+        }
+
+        public static int DecodeTimestamp(String timestamp)
+        {
+            return 0;
+        }
+
+        public static String EncodeIp(string ip)
+        {
+            IPAddress address = IPAddress.Parse(ip);
+            return Convert.ToBase64String(address.GetAddressBytes());
+        }
+
+        public static String DecodeIp(string ip)
+        {
+            byte[] ipNum = Convert.FromBase64String(ip);
+            IPAddress address = new IPAddress(ipNum);
+            return address.ToString();
+        }
+    }
+}
