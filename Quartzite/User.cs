63631e203d1c56179b059d24f9260d7da2c26fca
phate408 <phate408@gmail.com>
2013-11-25 17:59:55 -0800

    Added user classes

---
+++ Quartzite/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quartzite
+{
+    public class User : UserBase
+    {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Constructor
+        public User()
+        {
+            
+        }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
