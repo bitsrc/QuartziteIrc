b97622a7043ebdb8235e71b4ac2c42f874f65ba1
phate408 <phate408@gmail.com>
2013-11-25 18:00:26 -0800

    Added channel

---
+++ Quartzite/Channel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quartzite
+{
+    public class Channel
+    {
+        #region Fields
+        public List<UserBase> Users;
+        public List<Mode> Modes;
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Events
+
+        public event EventHandler OnMessage;
+        public event EventHandler OnNotice;
+        public event EventHandler OnJoin;
+        public event EventHandler OnPart;
+        public event EventHandler OnMode;
+        public event EventHandler OnTopicChange;
+
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
