47f3a3fdbfaadf37566b7c3fd3603696972c5cc0
phate408 <phate408@gmail.com>
2013-11-25 18:00:35 -0800

    Added mode

---
+++ Quartzite/Mode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quartzite
+{
+    public class Mode
+    {
+        #region Fields
+        public readonly Char Type;
+        public String Parameter;
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
